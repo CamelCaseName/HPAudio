d01194f5bc8e1b404b5eb84f06c1c0f49352abbb
Leonhard Seidel <leonhardseidel@gmx.de>
2022-09-26 23:26:09 +0200

    Sometimes it works, sometimes it doesnt

--- AudioMod.cs
+++ AudioMod.cs
@@ -1,16 +1,12 @@
-﻿using Il2CppSystem.Collections.Generic;
-using EekCharacterEngine;
-using EekCharacterEngine.Interaction;
-using Il2CppSystem.Net;
-using UnityEngine.InputSystem;
-using MelonLoader;
-using Object = UnityEngine.GameObject;
-using NLayer;
+﻿using EekCharacterEngine.Interaction;
 using HouseParty;
-using UnityEngine;
+using Il2CppSystem.Collections.Generic;
 using Il2CppSystem.IO;
-using UnityEngine.Networking;
-using Il2CppSystem;
+using MelonLoader;
+using NLayer;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Object = UnityEngine.Object;
 
 namespace HPAudio
 {
@@ -24,12 +20,14 @@ namespace HPAudio
         private int currentSong = 0;
         public bool CurrentlyPlaying = false;
         private bool gotSpeakers = false;
+        private float volume = 0.5f;
         private string currentSongName = "None";
 
         public override void OnApplicationStart()
         {
             folderPath = Directory.GetCurrentDirectory() + "\\HouseParty_Data\\Resources\\Songs";
             MelonLogger.Msg("Place songs (as .mp3) here: " + folderPath);
+            ReloadSongList();
         }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
@@ -96,6 +94,14 @@ namespace HPAudio
         }
 
         /// <summary>
+        /// Randomly selects the next song, then plays that.
+        /// </summary>
+        public void Shuffle()
+        {
+
+        }
+
+        /// <summary>
         /// Skips to the previous track in the list
         /// </summary>
         public void Previous()
@@ -157,6 +163,7 @@ namespace HPAudio
             MelonLogger.Msg(e.ToString());
 #endif
             MelonLogger.Msg($"'{currentSongName}' can't be loaded!");
+            CurrentlyPlaying = false;
         }
 
         private MpegFile LoadSongFromFile()
@@ -179,61 +186,90 @@ namespace HPAudio
             float[] samples = new float[songfile.Length / sizeof(float)];
             try
             {
+                songfile.StereoMode = StereoMode.DownmixToMono;
                 songfile.ReadSamples(samples, 0, samples.Length);
                 AudioClip clip = AudioClip.Create(currentSongName, samples.Length / songfile.Channels, songfile.Channels, songfile.SampleRate, false);
-                clip.SetData(samples, 1);
+                clip.SetData(samples, 0);
                 speaker1.GBPLLLIBAIO.clip = clip;
                 speaker2.GBPLLLIBAIO.clip = clip;
             }
-            catch (System.Runtime.CompilerServices.RuntimeWrappedException e)
+            catch (System.Runtime.CompilerServices.RuntimeWrappedException re)
             {
-                LogException(e.InnerException);
+                if (re.WrappedException != null)
+                {
+                    MelonLogger.Msg(re.WrappedException);
+                    MelonLogger.Msg(((Il2CppSystem.ArgumentException)re.WrappedException).m_paramName); 
+                    MelonLogger.Msg(((Il2CppSystem.ArgumentException)re.WrappedException).Message); 
+                    MelonLogger.Msg(((Il2CppSystem.ArgumentException)re.WrappedException).StackTrace); 
+                    MelonLogger.Msg(((Il2CppSystem.ArgumentException)re.WrappedException).ToString());
+                }
+                LogException(re);
                 return;
             }
+            catch (System.Exception e)
+            {
+                LogException(e);
+                return;
+            }
+            CurrentlyPlaying = true;
         }
 
         public void ManageSpeakerSettings()
         {
+            speaker1.GBPLLLIBAIO.time = 0;
+            speaker2.GBPLLLIBAIO.time = 0;
+            speaker1.GBPLLLIBAIO.priority = 32;
+            speaker2.GBPLLLIBAIO.priority = 32;
             speaker1.GBPLLLIBAIO.enabled = true;
             speaker2.GBPLLLIBAIO.enabled = true;
             speaker1.GBPLLLIBAIO.ignoreListenerPause = true;
             speaker2.GBPLLLIBAIO.ignoreListenerPause = true;
             speaker1.GBPLLLIBAIO.mute = false;
             speaker2.GBPLLLIBAIO.mute = false;
-            speaker1.GBPLLLIBAIO.Play();
-            speaker2.GBPLLLIBAIO.Play();
+            speaker1.GBPLLLIBAIO.bypassEffects = true;
+            speaker2.GBPLLLIBAIO.bypassEffects = true;
+            speaker1.GBPLLLIBAIO.bypassListenerEffects = true;
+            speaker2.GBPLLLIBAIO.bypassListenerEffects = true;
+            //speaker1.GBPLLLIBAIO.volume = volume;
+            //speaker2.GBPLLLIBAIO.volume = volume;
         }
 
         public override void OnUpdate()
         {
             if (inGameMain && Keyboard.current[Key.D].wasPressedThisFrame && Keyboard.current[Key.LeftAlt].isPressed)
             {
-                GetSpeakers();
+                //if the storys are downloaded
+                if (EekCharacterEngine.StoryManager.JJNLDMJHICJ)
+                {
+                    GetSpeakers();
 
-                speaker1.StopMusic();
-                speaker2.StopMusic();
+                    speaker1.GBPLLLIBAIO.Stop();
+                    speaker2.GBPLLLIBAIO.Stop();
 
-                if(songs.Count < 1) ReloadSongList();
+                    if (songs.Count < 1) ReloadSongList();
 
-                var songfile = LoadSongFromFile();
+                    var songfile = LoadSongFromFile();
 
-                Next();
+                    Next();
 
-                speaker1.GBPLLLIBAIO.Stop();
-                speaker2.GBPLLLIBAIO.Stop();
+                    speaker1.GBPLLLIBAIO.Stop();
+                    speaker2.GBPLLLIBAIO.Stop();
 
-                LoadSongIntoSpeakers(songfile);
+                    ManageSpeakerSettings();
 
-                ManageSpeakerSettings();
+                    LoadSongIntoSpeakers(songfile);
+                    if (!CurrentlyPlaying) return;
 
-                MelonLogger.Msg("Now playing: " + currentSongName);
+                    speaker1.GBPLLLIBAIO.Play();
+                    speaker2.GBPLLLIBAIO.Play();
 
-
-                MelonLogger.Msg(speaker1.GBPLLLIBAIO.volume);
-                MelonLogger.Msg(speaker1.GBPLLLIBAIO.pitch);
-                MelonLogger.Msg(speaker1.GBPLLLIBAIO.clip.length);
-                MelonLogger.Msg(speaker1.GBPLLLIBAIO.isPlaying);
-                MelonLogger.Msg(speaker1.GBPLLLIBAIO.isActiveAndEnabled);
+                    if (CurrentlyPlaying) MelonLogger.Msg("Now playing: " + currentSongName);
+                    MelonLogger.Msg($"vol: {speaker1.GBPLLLIBAIO.volume:0.00} | length {speaker1.GBPLLLIBAIO.clip.length / 60:0}m{speaker1.GBPLLLIBAIO.clip.length % 60:0.00}s");
+                }
+                else
+                {
+                    MelonLogger.Msg("Please wait for the game to finish its story file download and extraction");
+                }
             }
         }
     }
